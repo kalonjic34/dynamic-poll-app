aa9a705629b6ea45d12e142768902b8062e2a7f9
Christian <kalonjic34@gmail.com>
2026-09-07 14:58:43 +0200

    implement option voting functionality

--- resources/views/components/polls.blade.php
+++ resources/views/components/polls.blade.php
@@ -1,14 +1,22 @@
 <?php
 
 use Livewire\Component;
+use App\Models\Option;
 
 new class extends Component
 {
+    protected $listeners = [
+        'pollCreated' => 'render'
+    ];
 
     public function render()
     {
         $polls = \App\Models\Poll::with('options.votes')->latest()->get();
         return view('components.polls', ['polls' => $polls]);
+        }
+
+        public function vote(Option $option){
+            $option->votes()->create();
         }
 }
 ?>
@@ -19,7 +27,7 @@ new class extends Component
             <h3 class="mb-4 text-xl">{{ $poll->title }}</h3>
                 @foreach ( $poll->options as $option)
                     <div class="mb-2">
-                        <button class="btn">Vote</button>
+                        <button class="btn" wire:click="vote({{ $option->id }})">Vote</button>
                         {{ $option->name }} ({{ $option->votes->count() }})
                     </div>
                 @endforeach
